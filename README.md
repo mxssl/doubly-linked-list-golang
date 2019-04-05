3520599410f86f413a28d04a7844447f85eaa36e
mxssl <susula@ya.ru>
2019-04-06 02:46:35 +0300

    add tests

--- README.md
+++ README.md
@@ -3,9 +3,40 @@
 Doubly Linked List implementation in Go (Golang).
 
 ```bash
-go test -v ./...
-=== RUN   TestCase1
---- PASS: TestCase1 (0.00s)
+go test -v -cover ./...
+=== RUN   TestSingleNodeHead
+--- PASS: TestSingleNodeHead (0.00s)
+=== RUN   TestSingleNodeTail
+--- PASS: TestSingleNodeTail (0.00s)
+=== RUN   TestRemoveHead
+--- PASS: TestRemoveHead (0.00s)
+=== RUN   TestRemoveTail
+--- PASS: TestRemoveTail (0.00s)
+=== RUN   TestRemoveNodesWithValueHead
+--- PASS: TestRemoveNodesWithValueHead (0.00s)
+=== RUN   TestRemoveNodesWithValueTail
+--- PASS: TestRemoveNodesWithValueTail (0.00s)
+=== RUN   TestInsertAtPosition
+--- PASS: TestInsertAtPosition (0.00s)
+=== RUN   TestSetHeadAndTail
+--- PASS: TestSetHeadAndTail (0.00s)
+=== RUN   TestInsertAfterHead
+--- PASS: TestInsertAfterHead (0.00s)
+=== RUN   TestInsertBeforeHead
+--- PASS: TestInsertBeforeHead (0.00s)
+=== RUN   TestInsertAfterTail
+--- PASS: TestInsertAfterTail (0.00s)
+=== RUN   TestInsertBeforeTail
+--- PASS: TestInsertBeforeTail (0.00s)
+=== RUN   TestInsertAtPositionHeadTail
+--- PASS: TestInsertAtPositionHeadTail (0.00s)
+=== RUN   TestContainsNodeWithValueHead
+--- PASS: TestContainsNodeWithValueHead (0.00s)
+=== RUN   TestContainsNodeWithValueTail
+--- PASS: TestContainsNodeWithValueTail (0.00s)
+=== RUN   TestContainsNodeWithValue
+--- PASS: TestContainsNodeWithValue (0.00s)
 PASS
-ok      github.com/mxssl/doubly-linked-list-golang      0.006s
+coverage: 86.7% of statements
+ok      github.com/mxssl/doubly-linked-list-golang      0.006s  coverage: 86.7% of statements
 ```
--- main_test.go
+++ main_test.go
@@ -2,15 +2,162 @@ package doublylinkedlist
 
 import "testing"
 
-func TestCase1(t *testing.T) {
+func TestSingleNodeHead(t *testing.T) {
 	dll := NewDoublyLinkedList()
-	node := NewNode(1)
+	node := newNode(1)
 
 	dll.setHead(node)
 	expectingSingleNode(t, dll, node)
 }
 
-func NewNode(value int) *Node {
+func TestSingleNodeTail(t *testing.T) {
+	dll := NewDoublyLinkedList()
+	node := newNode(1)
+
+	dll.setTail(node)
+	expectingSingleNode(t, dll, node)
+}
+
+func TestRemoveHead(t *testing.T) {
+	dll := NewDoublyLinkedList()
+	node := newNode(1)
+
+	dll.setHead(node)
+	dll.remove(node)
+	expectingEmpty(t, dll)
+}
+
+func TestRemoveTail(t *testing.T) {
+	dll := NewDoublyLinkedList()
+	node := newNode(1)
+
+	dll.setTail(node)
+	dll.remove(node)
+	expectingEmpty(t, dll)
+}
+
+func TestRemoveNodesWithValueHead(t *testing.T) {
+	dll := NewDoublyLinkedList()
+	node := newNode(1)
+
+	dll.setHead(node)
+	dll.removeNodesWithValue(1)
+	expectingEmpty(t, dll)
+}
+
+func TestRemoveNodesWithValueTail(t *testing.T) {
+	dll := NewDoublyLinkedList()
+	node := newNode(1)
+
+	dll.setTail(node)
+	dll.removeNodesWithValue(1)
+	expectingEmpty(t, dll)
+}
+
+func TestInsertAtPosition(t *testing.T) {
+	dll := NewDoublyLinkedList()
+	node := newNode(1)
+
+	dll.insertAtPosition(1, node)
+	expectingSingleNode(t, dll, node)
+}
+
+func TestSetHeadAndTail(t *testing.T) {
+	dll := NewDoublyLinkedList()
+	first := newNode(1)
+	second := newNode(2)
+
+	dll.setHead(first)
+	dll.setTail(second)
+	expectingHeadTail(t, dll, first, second)
+}
+
+func TestInsertAfterHead(t *testing.T) {
+	dll := NewDoublyLinkedList()
+	first := newNode(1)
+	second := newNode(2)
+
+	dll.setHead(first)
+	dll.insertAfter(first, second)
+	expectingHeadTail(t, dll, first, second)
+}
+
+func TestInsertBeforeHead(t *testing.T) {
+	dll := NewDoublyLinkedList()
+	first := newNode(1)
+	second := newNode(2)
+
+	dll.setHead(first)
+	dll.insertBefore(first, second)
+	expectingHeadTail(t, dll, second, first)
+}
+
+func TestInsertAfterTail(t *testing.T) {
+	dll := NewDoublyLinkedList()
+	first := newNode(1)
+	second := newNode(2)
+
+	dll.setTail(first)
+	dll.insertAfter(first, second)
+	expectingHeadTail(t, dll, first, second)
+}
+
+func TestInsertBeforeTail(t *testing.T) {
+	dll := NewDoublyLinkedList()
+	first := newNode(1)
+	second := newNode(2)
+
+	dll.setHead(first)
+	dll.insertBefore(first, second)
+	expectingHeadTail(t, dll, second, first)
+}
+
+func TestInsertAtPositionHeadTail(t *testing.T) {
+	dll := NewDoublyLinkedList()
+	first := newNode(1)
+	second := newNode(2)
+
+	dll.insertAtPosition(1, first)
+	dll.insertAtPosition(2, second)
+	expectingHeadTail(t, dll, first, second)
+}
+
+func TestContainsNodeWithValueHead(t *testing.T) {
+	dll := NewDoublyLinkedList()
+	node := newNode(1)
+
+	dll.setHead(node)
+	if !dll.containsNodeWithValue(1) {
+		t.Fail()
+	}
+}
+
+func TestContainsNodeWithValueTail(t *testing.T) {
+	dll := NewDoublyLinkedList()
+	node := newNode(1)
+
+	dll.setTail(node)
+	if !dll.containsNodeWithValue(1) {
+		t.Fail()
+	}
+}
+
+func TestContainsNodeWithValue(t *testing.T) {
+	dll := NewDoublyLinkedList()
+	first := newNode(1)
+	second := newNode(2)
+	third := newNode(3)
+
+	dll.setHead(first)
+	dll.insertAfter(first, second)
+	dll.insertAfter(second, third)
+
+	if !dll.containsNodeWithValue(2) {
+		t.Fail()
+	}
+}
+
+func newNode(value int) *Node {
 	return &Node{
 		value: value,
 	}
@@ -21,6 +168,24 @@ func expectingSingleNode(t *testing.T, dll *DoublyLinkedList, node *Node) {
 		t.Fail()
 	}
 	if dll.tail != node {
+		t.Fail()
+	}
+}
+
+func expectingEmpty(t *testing.T, dll *DoublyLinkedList) {
+	if dll.head != nil {
+		t.Fail()
+	}
+	if dll.tail != nil {
+		t.Fail()
+	}
+}
+
+func expectingHeadTail(t *testing.T, dll *DoublyLinkedList, head *Node, tail *Node) {
+	if dll.head != head {
+		t.Fail()
+	}
+	if dll.tail != tail {
 		t.Fail()
 	}
 }
